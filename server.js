5560a3f64fa5afa3195c5d48a11c4d65ef7ae3a1
Nikola <perisicnikola37@gmail.com>
2023-06-16 17:50:45 +0200

    Finish cookie login

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 // MongoDB connection
 // const mongoURI = 'mongodb+srv://nikola:06032004@cluster0.4uutsny.mongodb.net/vjezbanje';
@@ -27,6 +29,52 @@ const User = mongoose.model('User', {
     password: String,
 });
 
+const authenticateToken = async (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        res.status(401).send('Access token not provided.');
+        return;
+    }
+
+    jwt.verify(token, 'token', async (error, decodedToken) => {
+
+        if (error !== null && error !== undefined) {
+            res.status(500).json({
+                msg: 'Invalid token',
+                status: "Auth not valid"
+            });
+            return;
+        }
+
+        console.log(decodedToken, "DEKODIRANI TOKEN")
+        // Verify token version
+
+        const user = await User.findOne({ username: decodedToken.username })
+
+        res.status(200).json({
+            user,
+            msg: "Valid session"
+        })
+
+
+
+        // User.findOne({ username: decodedToken.username })
+        //     .then((user) => {
+        //         if (!user || user.tokenVersion !== decodedToken.tokenVersion) {
+        //             res.status(403).send('Invalid token.');
+        //             return;
+        //         }
+
+        //         req.user = decodedToken;
+        //         next();
+        //     })
+        //     .catch((error) => {
+        //         console.error('Error verifying token', error);
+        //         res.status(500).send('An error occurred while verifying token.');
+        //     });
+    });
+}
 
 // Routes
 app.get('/', (req, res) => {
@@ -49,17 +97,32 @@ app.post('/register', (req, res) => {
 });
 
 // Login route
+// Login route
+// Login route
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    User.findOne({ username, password })
+    User.findOne({ username })
         .then((user) => {
             if (!user) {
                 res.status(401).send('Invalid username or password.');
                 return;
             }
 
-            const token = jwt.sign({ username, role: user.role }, 'secret_key');
+            if (user.password !== password) {
+                // User's password is incorrect
+                res.status(401).send('Invalid username or password.');
+                return;
+            }
+
+            // Generate a new token
+            const tokenVersion = user.tokenVersion || 0;
+            const token = jwt.sign({ username, role: user.role, tokenVersion }, 'token');
+
+            // Set the token in a cookie named "token" with an expiration of 30 days
+            const expirationDate = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000); // 30 days
+            res.cookie('token', token, { expires: expirationDate });
+
             res.send({ token });
         })
         .catch((error) => {
@@ -80,32 +143,21 @@ app.delete('/users', (req, res) => {
         });
 });
 
+// Logout route
+app.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    res.send('Logged out successfully!');
+});
+
 
 // Protected route
 app.get('/protected', authenticateToken, (req, res) => {
-    res.send('Protected route!');
+    res.send('HELLO WORLD!');
 });
 
 // Token authentication middleware
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
 
-    if (!token) {
-        res.status(401).send('Access token not provided.');
-        return;
-    }
 
-    jwt.verify(token, 'secret_key', (error, user) => {
-        if (error) {
-            res.status(403).send('Invalid token.');
-            return;
-        }
-
-        req.user = user;
-        next();
-    });
-}
 
 // Start the server
 app.listen(port, () => {
