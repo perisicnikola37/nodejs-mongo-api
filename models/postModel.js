9b7f388ab01bd670f05f2ef9e23f7fe1ce5989e2
Nikola <perisicnikola37@gmail.com>
2023-06-20 14:58:46 +0200

    Create postModel.js file

---
+++ models/postModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Post = mongoose.model('post', postShema)
+
+module.exports = Post
