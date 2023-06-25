b010b0806cd90f4e4e1383c75ac9a15d1788389d
Nikola <perisicnikola37@gmail.com>
2023-06-25 03:24:39 +0200

    Finish API docs for user routes

--- app.js
+++ app.js
@@ -4,21 +4,42 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const axios = require('axios');
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
 
+// Swagger options
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Node.js API',
+            version: '1.0.0',
+            description: 'API documentation for Node.js API',
+        },
+    },
+    apis: [
+        './routes/userRoutes.js',
+        './routes/postRoutes.js',
+    ],
+};
+
+// Initialize Swagger-jsdoc
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+// Serve Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 // Routes
-app.use("/api/v1", userRoutes);
-app.use("/api/v1", postRoutes);
+app.use('/api/v1', userRoutes);
+app.use('/api/v1', postRoutes);
 
-// Main route
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
+const port = process.env.PORT || 3000;
 
-module.exports = app;
+module.exports = app
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,112 @@ const router = express.Router();
 const { registerUser, loginUser, deleteUsers, protectedRoute, logout } = require('./../controllers/UserController');
 const { authenticateToken } = require('../middleware/middleware');
 
+// Import swagger-jsdoc and swagger-ui-express
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+// Swagger options
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Node.js API',
+            version: '1.0.0',
+            description: 'API documentation for Node.js API',
+        },
+    },
+    apis: ['./routes/userRoutes.js'], // Specify the file containing the API operations
+};
+
+// Initialize swagger-jsdoc
+const swaggerSpec = swaggerJsDoc(swaggerOptions);
+
+// Serve Swagger UI
+router.use('/docs', swaggerUi.serve);
+router.get('/docs', swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /api/v1/register:
+ *   post:
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: Successful registration
+ *       400:
+ *         description: Invalid request data
+ */
+
 router.post('/register', registerUser);
+
+/**
+ * @swagger
+ * /api/v1/login:
+ *   post:
+ *     summary: Log in user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LoginRequest'
+ *     responses:
+ *       200:
+ *         description: Successful login
+ *       401:
+ *         description: Unauthorized
+ */
+
 router.post('/login', loginUser);
+
+/**
+ * @swagger
+ * /api/v1/delete-users:
+ *   delete:
+ *     summary: Delete all users
+ *     responses:
+ *       200:
+ *         description: Successful deletion
+ *       401:
+ *         description: Unauthorized
+ */
+
 router.delete('/delete-users', deleteUsers);
+
+/**
+ * @swagger
+ * /api/v1/protected-route:
+ *   get:
+ *     summary: Protected route
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successful request
+ *       401:
+ *         description: Unauthorized
+ */
+
 router.get('/protected-route', authenticateToken, protectedRoute);
+
+/**
+ * @swagger
+ * /api/v1/logout:
+ *   get:
+ *     summary: Log out user
+ *     responses:
+ *       200:
+ *         description: Successful logout
+ *       401:
+ *         description: Unauthorized
+ */
+
 router.get('/logout', logout);
 
 module.exports = router;
