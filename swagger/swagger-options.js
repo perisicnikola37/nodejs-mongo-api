dce0ab840a08d5cfb62169a99b8afa1c246afe5f
Nikola <perisicnikola37@gmail.com>
2023-06-25 22:16:56 +0200

    Add User schema definition to swagger documentation

--- swagger/swagger-options.js
+++ swagger/swagger-options.js
@@ -1,20 +1,3 @@
-const express = require('express');
-const app = express();
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-const axios = require('axios');
-require('dotenv').config();
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
-
-const userRoutes = require('./routes/userRoutes');
-const postRoutes = require('./routes/postRoutes');
-
-// Middleware
-app.use(express.json());
-app.use(cookieParser());
-
-// Swagger options
 const swaggerOptions = {
     definition: {
         openapi: '3.0.0',
@@ -50,16 +33,4 @@ const swaggerOptions = {
     ],
 };
 
-// Initialize Swagger-jsdoc
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-
-// Serve Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// Routes
-app.use('/api/v1', userRoutes);
-app.use('/api/v1', postRoutes);
-
-const port = process.env.PORT || 3000;
-
-module.exports = app
+module.exports = swaggerOptions
